dbea7b71334bcd02fa5373b6729dc16143dc02eb
Rafael Ponte <rafael.ponte@zup.com.br>
2021-04-07 18:37:30 -0300

    roteiro do checkout estendido;

--- src/test/kotlin/br/com/zup/edu/propostas/CreateProposalEndpointTest.kt
+++ src/test/kotlin/br/com/zup/edu/propostas/CreateProposalEndpointTest.kt
@@ -21,6 +21,36 @@ import org.junit.jupiter.api.assertThrows
 import java.math.BigDecimal
 import java.util.*
 
+/**
+ * ROTEIRO do Checkout Estendido:
+ *
+ * 1. contextualização: apresentação da aplicação
+ *  1.1. explica e navega pelo codigo
+ *  1.2. testa manualmente com BloomRPC
+ * 2. introdução a testes, tipos de testes
+ * 3. unidade X integração
+ * 4. criando classe de teste com @MicronautTest
+ * 5. executando teste (vazio) e entendendo o contexto do micronaut (banco, server grpc etc)
+ *  5.1. configurando datasource de testes: H2
+ * 6. escreve teste happy-path "deve criar nova proposta"
+ *  6.1. 3 blocos de um teste
+ *  6.2. fabricando o gRPC client com @Factory, ip/porta do channel etc
+ *  6.3. injetando gRPC client
+ *  6.4. validando efeitos colateriais: assertTrue(repository.existsById(UUID.fromString(id)))
+ *  6.5. entendendo o problema de usar @Transactional em endpoints gRPC
+ *  6.6. avaliando alternativas: auto-commit, Service e SynchronousTransactionManager
+ * 7. escreve teste alternativo "nao deve criar proposta quando documento existente"
+ *  6.1. validando somente contrato da API (status code e description)
+ *  6.2. por que quebrou? entendendo @MicronautTest(transactional = false)
+ *  6.3. (opcional) habilitando logs de transacoes
+ * 8. escreve teste alternativo "nao deve criar propostas quando parametros de entrada forem invalidos"
+ *  8.1. validando somente contrato da API (status code e description)
+ *  8.2. validando violações da Bean Validation com Hamcrest .hasItem()
+ *  8.3. validando violações da Bean Validation com Hamcrest .containsInAnyOrder()
+ *  8.3. melhorando com Extension Functions: StatusRuntimeException.violations()
+ * 9. evitando repetição de código com @BeforeEach
+ * 10. duvidas?
+ */
 @MicronautTest(transactional = false)
 internal class CreateProposalEndpointTest(
     val repository: ProposalRespository,
@@ -112,7 +142,10 @@ internal class CreateProposalEndpointTest(
         }
     }
 
-    fun StatusRuntimeException.violations(): List<Pair<String, String>>? {
+    /**
+     * TODO: mover para arquivo separado para ser reutilizado entre vários testes
+     */
+    private fun StatusRuntimeException.violations(): List<Pair<String, String>>? {
         val details = StatusProto.fromThrowable(this)
             ?.detailsList?.get(0)!!
             .unpack(BadRequest::class.java)
